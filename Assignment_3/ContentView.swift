6cddb829e936790a6448ebc501382205aabe18a6
John Vickers <47126229+QuinleyV@users.noreply.github.com>
2026-04-20 07:08:49 -0500

    Update ContentView.swift

--- Assignment_3/ContentView.swift
+++ Assignment_3/ContentView.swift
@@ -1,1 +1,17 @@
+import SwiftUI
 
+struct ContentView: View {
+    @StateObject private var authViewModel = AuthViewModel()
+    
+    var body: some View {
+        Group {
+            if authViewModel.user != nil {
+                DiaryHomeView()
+                    .environmentObject(authViewModel)
+            } else {
+                LoginView()
+                    .environmentObject(authViewModel)
+            }
+        }
+    }
+}
