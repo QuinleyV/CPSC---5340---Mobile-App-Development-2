757a93fc900ed2c0262aeec2be3df8f00212605b
John Vickers <47126229+QuinleyV@users.noreply.github.com>
2026-04-20 07:07:09 -0500

    Update DiaryApp.swift

--- Assignment_3/DiaryApp.swift
+++ Assignment_3/DiaryApp.swift
@@ -1,1 +1,16 @@
+import SwiftUI
+import FirebaseCore
 
+@main
+struct MyDiaryApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
